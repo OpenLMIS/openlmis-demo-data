3cec19362b39e6009206682507789780198cfbd4
Chongsun Ahn <cahn922@yahoo.com>
2018-06-06 12:55:00 -0700

    OLMIS-4701 Drop existing schema before creating
    Make script a bit smarter by trying to drop any existing schema before creating it.

--- load.sh
+++ load.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# For each schema to load, need to drop any existing schema before loading
 for file in data/schema/schema*.sql; do
+
+    # See http://tldp.org/LDP/abs/html/string-manipulation.html
+    noprefix=${file#*.}       # Remove prefix inclusively up to first dot
+    schemaname=${noprefix%.*} # Remove suffix inclusively up to last dot
+
+    psql -c "drop schema if exists ${schemaname} cascade;" 2>&1
     psql -f $file
 done
 
