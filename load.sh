4198b067986e32bf4c90f9e28e5db4487d722499
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-12-21 11:46:50 +0100

    Revert "psql will stop on first error"
    This reverts commit 5f1780b0d20b1cf68b8989c089b18e9891dbbd15.

--- load.sh
+++ load.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-PSQL="psql --single-transaction --set AUTOCOMMIT=off --set ON_ERROR_STOP=on"
-
 # For each schema to load, need to drop any existing schema before loading
 for file in data/schema/schema*.sql; do
 
@@ -9,8 +7,8 @@ for file in data/schema/schema*.sql; do
     noprefix=${file#*.}       # Remove prefix inclusively up to first dot
     schemaname=${noprefix%.*} # Remove suffix inclusively up to last dot
 
-    ${PSQL} -c "drop schema if exists ${schemaname} cascade;" 2>&1
-    ${PSQL} -f $file
+    psql -c "drop schema if exists ${schemaname} cascade;" 2>&1
+    psql -f $file
 done
 
-${PSQL} -f data/load_data.sql
+psql -f data/load_data.sql
