f143c930c828c87c2ccc3f8141687796b9ea046b
Ian Manyama <ian.manyama@tz-ghsc.com>
2019-07-07 17:49:57 +0300

    OLMIS-6375 : Fix typos

--- casper/load_data.sql
+++ casper/load_data.sql
@@ -1,6 +1,6 @@
 --
 -- This program is part of the OpenLMIS logistics management information system platform software.
--- Copyright © 2013 VillageReach
+-- Copyright © 2019 VillageReach
 --
 -- This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 --
@@ -40,9 +40,6 @@ INSERT INTO referencedata.geographic_zones
 INSERT INTO referencedata.geographic_zones
 (id, code, name, levelId, parentId) values
 ('cc125c1f-e335-4685-99f3-e85a889e17dd', 'Ngorongoro', 'Ngorongoro', (SELECT id from referencedata.geographic_levels WHERE code = 'district'), (SELECT id from referencedata.geographic_zones WHERE code = 'Dodoma'));
-
-
-
 
 
 
@@ -98,15 +95,6 @@ INSERT INTO referencedata.supported_programs (facilityId, programId, startDate, 
     WHERE code = 'ilshosp'), '11/11/12', TRUE);
 
 
---
--- This program is part of the OpenLMIS logistics management information system platform software.
--- Copyright © 2013 VillageReach
---
--- This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
--- You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
---
 insert into referencedata.orderable_display_categories
 (id, code, displayname , displayOrder) values
    ('0704c1ec-c12e-4ff2-aaf6-5063828d4937','arv','Antiretrovirals',1),
